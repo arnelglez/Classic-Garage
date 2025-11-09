407319eef15ba36962550f0d8e2c1e318b15a697
Arnel Gonzalez <arnel.glez@gmail.com>
2025-11-08 22:18:38 -0500

    Add menu configuration for site navigation

---
+++ src/const/menu.ts
@@ -0,0 +1,30 @@
+import type { Menu } from "../interfaces/interfaces";
+
+export const menus: Menu[] = [
+    {
+        title:"About Us",
+        href:"#about" ,
+        section:"about",
+    },
+    {
+        title:"Featured Services",
+        href:"#featured" ,
+        section:"featured",
+    },
+    {
+        title:"Services",
+        href:"#services", 
+        section:"services",
+    },
+    {
+        title:"Testimonials",
+        href:"#testimonials",
+        section:"testimonials",
+    },
+    {
+        title:"Contact",
+        href:"#contact",
+        section:"contact"
+    }
+];
+
